31abb52e1824ea5a7a6cf77b1ae97bcdf71a1198
ilanhalioua <113393627+ilanhalioua@users.noreply.github.com>
2023-05-14 21:15:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ basic_mdp = setUp_mdpSolver(file)
 V = basic_mdp.ValueIteration()
 policy = basic_mdp.OptimalPolicy(V)
 
+# Print the Value function for each state
+print("\nValue function for each state:\n")
+for state in V:
+    print(f"V( {state} ) = {V[state]}")
+
 # Print the resulting policy
+print("\n\nOptimal Policy:\n")
 for state in policy:
-    print(f"State: {state}, Action: {policy[state]}")
+    print(f"{state} -> Optimal: {policy[state]}")

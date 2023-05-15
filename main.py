8f22bd4a2c8fa13ce56ccbaaed259060b79e07f8
ilanhalioua <113393627+ilanhalioua@users.noreply.github.com>
2023-05-15 21:25:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from setUp import setUp_mdpSolver, askFile
 
 file = askFile()
-basic_mdp = setUp_mdpSolver(file)
-V = basic_mdp.ValueIteration()
-policy = basic_mdp.OptimalPolicy(V)
+user_mdp = setUp_mdpSolver(file)
+if user_mdp.States and user_mdp.Actions and user_mdp.Transitions and user_mdp.ImmediateCosts:
+    V = user_mdp.ValueIteration()
+    policy = user_mdp.OptimalPolicy(V)
+    user_mdp.WriteOptimalPolicy(policy)
 
-# Print the Value function for each state
-print("\nValue function for each state:\n")
-for state in V:
-    print(f"V( {state} ) = {V[state]}")
+    # Print the Value function for each state
+    print("\nValue function for each state:\n")
+    for state in V:
+        print(f"V( {state} ) = {V[state]}")
 
-# Print the resulting policy
-print("\n\nOptimal Policy:\n")
-for state in policy:
-    print(f"{state} -> Optimal: {policy[state]}")
+    # Print the resulting policy
+    print("\n\nOptimal Policy:\n")
+    for state in policy:
+        print(f"{state} -> Optimal: {policy[state]}")
+
+else:
+    print("\nIntroduce your mdp definition with the expected file format, please!")

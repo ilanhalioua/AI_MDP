a703c3cf925132491bc38ca362f7175cb0c35be8
ilanhalioua <113393627+ilanhalioua@users.noreply.github.com>
2023-05-16 15:27:32 +0200

    Update and rename parser.py to Source Code/parser.py

--- Source Code/parser.py
+++ Source Code/parser.py
@@ -18,7 +18,7 @@ def parse_file(file):
             if line.lower().startswith('states:'):
                 # print()
                 # Use regular expressions to find all states in the line
-                matches = re.findall(r'([\w.]+(?:)?\*?)', line)[1:]
+                matches = re.findall(r'([\w.]+(?:)?\*?)', line)[1:] # match one or more word characters or periods (user may introduce states with ‘decimal’ format as in our thermostat problem), optionally followed by an asterisk (which will serve as a flag for the goal state).
                 for match in matches:
                     # Create a State object for each state found
                     val = match.rstrip('*')
@@ -35,7 +35,7 @@ def parse_file(file):
             elif line.lower().startswith('actions:'):
                 # print()
                 # Extract the list of actions
-                action_names = re.findall(r'\b[\w.]+\b', line)[1:]
+                action_names = re.findall(r'\b[\w.]+\b', line)[1:] # match whole words consisting of word characters or periods
                 for name in action_names:
                     action = Action(name)
                     # print(action)
@@ -45,14 +45,14 @@ def parse_file(file):
             elif line.lower().startswith('costs:'):
                 # print()
                 # Extract the costs
-                costs = re.findall(r'c\(\s*([\w.]+)\)\s*=\s*(\d+(?:\.\d+)?)', line)
+                costs = re.findall(r'c\(\s*([\w.]+)\)\s*=\s*(\d+(?:\.\d+)?)', line) # match a cost expression in the form of 'c(name) = value', where 'name' represents the cost name (composed of word characters or periods) and 'value' represents the cost value (a numeric value)
                 # Convert the costs to a dictionary
                 costs = {Action(action): float(cost) for action, cost in costs}
                 # print("IC = " + str(costs))
             # Match the lines that start with "transitions:"
             elif line.lower().startswith('transitions:'):
                 # print()
-                matches = re.findall(r'T\(\s*([\w.]+(?:)?\*?),\s*([\w.]+(?:)?\*?),\s*([\w.]+)\)\s*=\s(\d+(?:\.\d+)?)', line)
+                matches = re.findall(r'T\(\s*([\w.]+(?:)?\*?),\s*([\w.]+(?:)?\*?),\s*([\w.]+)\)\s*=\s(\d+(?:\.\d+)?)', line) # match a transitions and their respective probabilities when found in the form of 'T(name1, name2, name3) = value', where 'name1', 'name2', and 'name3' represent the parameters (composed of word characters or periods) and 'value' represents the numeric value (probability) assigned to the transition.
                 for match in matches:
                     p_val = match[0].rstrip('*')
                     p_goal = 1 if match[0].endswith('*') else 0
